64c56264c65072fab88b4d4c9626edd308a4126b
Gihan Chamila <algihanchamila@gmail.com>
2024-06-03 19:14:40 +0530

    User model created

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const userSchema = mongoose.Schema({
+    name: { type : String, required : true},
+    email: {type : String, required : true, unique : true, trim : true},
+    password: {type : String, required : true, minlength : 6},
+    // role 1 : super-admin, role 2 : admin, role 3 : user
+    role: {type : Number, default : 3}
+}, {
+    timestamps : true
+});
+
+const User = mongoose.model("user", userSchema)
+module.exports = User
