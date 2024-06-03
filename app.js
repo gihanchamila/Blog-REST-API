c7afca9434795522ed5ab8a60261754274e98bd1
Gihan Chamila <algihanchamila@gmail.com>
2024-06-03 22:36:54 +0530

    Created signup router

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const morgan = require("morgan")
 
 dotenv.config();
 const connectMongodb = require("./init/mongodb")
+const { authRoute } = require("./routes")
 
 // init app
 const app = express();
@@ -14,5 +16,9 @@ connectMongodb()
 // third-party middleware
 app.use(express.json({limit : "500mb"}));
 app.use(bodyParser.urlencoded({limit : "500mb", extended : true}));
+app.use(morgan("dev"))
+
+// route section
+app.use("/api/v1/auth", authRoute)
 
 module.exports = app;
