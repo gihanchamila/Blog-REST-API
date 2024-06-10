699265486875cd848ec62e761ab1979138bef97f
Gihan Chamila <algihanchamila@gmail.com>
2024-06-10 18:08:32 +0530

    Installed multer

--- routes/file.js
+++ routes/file.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const isAuth = require("../middlewares/isAuth")
 const {fileController} = require("../controllers")
+const multer = require("multer")
 
 router.post("/upload", isAuth, fileController.uploadFile )
 
