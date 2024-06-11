1192fbed230c3f871250f90a9e4e5896dbea9bee
Gihan Chamila <algihanchamila@gmail.com>
2024-06-11 12:53:55 +0530

    Moved local upload.js to backup folder

---
+++ backup/upload.js
@@ -0,0 +1,43 @@
+const path = require("path")
+const generateCode = require("../utils/generateCode")
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, "./uploads")
+    },
+
+    filename: (req, file, callback) => {
+        // original_file_name_12_digit_random_number.ext
+        const mimetype = file.mimetype;
+
+        if(
+            mimetype === "image/jpg"  ||
+            mimetype === "image/jpeg" ||
+            mimetype === "image/png"  ||
+            mimetype === "application/pdf"
+        ){
+            const originalName = file.originalname;
+            const extension = path.extname(originalName)
+            const fileName = originalName.replace(extension, "")
+            const compressedFilename = fileName.split(" ").join("_")
+            const lowercaseFilename = compressedFilename.toLocaleLowerCase();
+            const code = generateCode(12)
+            const finalFile = `${lowercaseFilename}_${code}${extension}`
+
+            callback(null, finalFile)
+           
+        }else {
+            callback(
+                new Error("Only jpg or jpeg or png or pdf format is allowed")
+            )
+        }
+
+        
+    }
+});
+
+const upload = multer({
+    storage
+});
+
+module.exports = upload
